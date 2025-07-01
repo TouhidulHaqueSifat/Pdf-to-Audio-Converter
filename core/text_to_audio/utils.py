87ed731f1742712fea8e7ea25402c412b6128cad
Touhidul Haque Sifat <touhidul01752@gmail.com>
2025-07-01 12:26:48 +0600

    Adding core functionality Extract text from pdf, convert the text into audio

---
+++ core/text_to_audio/utils.py
@@ -0,0 +1,30 @@
+from pypdf import PdfReader
+import pyttsx3
+
+class ExtarctText:
+
+    def extract_text_from_pdf(self, pdf_path,start_page,end_page):
+        reader = PdfReader(pdf_path)
+        text = ""
+        number_of_page = len(reader.pages)
+        if start_page < 0 or end_page >=number_of_page or start_page > end_page:
+            raise ValueError("Invalid page range")
+        
+        for i in range(start_page, end_page+1):
+            number_of_page = len(reader.pages)
+
+            if i < number_of_page:
+                text += reader.pages[i].extract_text() or ""
+        return text
+            
+class ConvertToAudio:
+    
+    def __init__(self):
+        self.engine = pyttsx3.init()
+
+    def convert_to_audio(self,text,filename):
+       self.engine.setProperty('rate',150)
+       self.engine.setProperty('volume',1.0)
+       self.engine.save_to_file(text,filename)
+       self.engine.runAndWait()
+
