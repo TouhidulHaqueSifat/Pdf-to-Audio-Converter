decd9a0c789f8dd017b17a47a06423d61426572d
Touhidul Haque Sifat <touhidul01752@gmail.com>
2025-07-01 15:14:48 +0600

    models created

--- core/text_to_audio/models.py
+++ core/text_to_audio/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class PdfModel(models.Model):
+    title = models.CharField(max_length=255)
+    pdf_file = models.FileField(upload_to='pdfs/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class AudioFile(models.Model):
+    pdf = models.ForeignKey(
+        PdfModel,on_delete=models.CASCADE,related_name='audio'
+        )
+    start_page = models.IntegerField()
+    end_page = models.IntegerField()
+    audio_file = models.FileField(upload_to ='audio/', null = True, blank = True)
+    created_at = models.DateTimeField(auto_now_add= True)
+    
+
